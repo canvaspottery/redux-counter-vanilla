ac403bbe1627a816587d0c457b1c363f03e26017
canvaspottery <canvaspottery@gmail.com>
2020-07-31 18:50:12 -0400

    copy basic example

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import { createStore } from "redux";
+
+function counter(state, action) {
+  if (typeof state === "undefined") {
+    return 0;
+  }
+
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+const store = createStore(counter);
+
+document.getElementById("increment").addEventListener("click", function () {
+  store.dispatch({ type: "INCREMENT" });
+});
