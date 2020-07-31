0686988b6af64056ade0f11a556e080784f86170
canvaspottery <canvaspottery@gmail.com>
2020-07-31 19:00:36 -0400

    delete decrement case

--- src/index.js
+++ src/index.js
@@ -12,8 +12,6 @@ function counter(state, action) {
   switch (action.type) {
     case INCREMENT:
       return state + 1;
-    case DECREMENT:
-      return state - 1;
     default:
       return state;
   }
