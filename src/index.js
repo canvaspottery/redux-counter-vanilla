d0e440fdbd1984a4e88bd9a521729152b60b0b8f
canvaspottery <canvaspottery@gmail.com>
2020-07-31 19:15:51 -0400

    use createSlice

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
-import { configureStore, createAction, createReducer } from "@reduxjs/toolkit";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-const increment = createAction("INCREMENT");
-const decrement = createAction("DECREMENT");
-
-const counter = createReducer(0, {
-  [increment]: (state) => state + 1,
-  [decrement]: (state) => state - 1,
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: 0,
+  reducers: {
+    increment: (state) => state + 1,
+    decrement: (state) => state - 1,
+  },
 });
 
-const store = configureStore({ reducer: counter });
+const store = configureStore({ reducer: counterSlice.reducer });
 const count = document.getElementById("count");
 count.innerHTML = store.getState();
 
 document.getElementById("increment").addEventListener("click", function () {
-  store.dispatch(increment());
+  store.dispatch(counterSlice.actions.increment());
 });
 
 document.getElementById("decrement").addEventListener("click", function () {
-  store.dispatch(decrement());
+  store.dispatch(counterSlice.actions.decrement());
 });
 
 store.subscribe(updateView);
